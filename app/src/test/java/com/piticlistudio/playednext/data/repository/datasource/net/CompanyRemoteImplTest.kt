0fd0e65becb89bb00da1fc77141a91b25fbceeb9
Jorge <piticlistudio@gmail.com>
2017-10-25 15:06:43 +0200

    Add genre repository implementation
    Adds GenreDataSourceRepository implementation for DAO and remote data layers.

--- app/src/test/java/com/piticlistudio/playednext/data/repository/datasource/net/CompanyRemoteImplTest.kt
+++ app/src/test/java/com/piticlistudio/playednext/data/repository/datasource/net/CompanyRemoteImplTest.kt
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 
-internal class CompanyRemoteImplTest() {
+internal class CompanyRemoteImplTest {
 
     @Nested
     @DisplayName("Given CompanyRemoteImpl instance")
