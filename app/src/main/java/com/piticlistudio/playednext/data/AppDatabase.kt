efe9c28470d0cbf4aab5508a6e22c67d4e44ad5b
Jorge <piticlistudio@gmail.com>
2017-10-25 10:33:48 +0200

    Adds GamePublisherDao
    Adds publisher dao entity.

--- app/src/main/java/com/piticlistudio/playednext/data/AppDatabase.kt
+++ app/src/main/java/com/piticlistudio/playednext/data/AppDatabase.kt
@@ -5,10 +5,11 @@ import android.arch.persistence.room.RoomDatabase
 import com.piticlistudio.playednext.data.entity.dao.CompanyDao
 import com.piticlistudio.playednext.data.entity.dao.GameDao
 import com.piticlistudio.playednext.data.entity.dao.GameDeveloperDao
+import com.piticlistudio.playednext.data.entity.dao.GamePublisherDao
 import com.piticlistudio.playednext.data.repository.datasource.dao.CompanyDaoService
 import com.piticlistudio.playednext.data.repository.datasource.dao.GameDaoService
 
-@Database(entities = arrayOf(GameDao::class, CompanyDao::class, GameDeveloperDao::class), version = 2, exportSchema = false)
+@Database(entities = arrayOf(GameDao::class, CompanyDao::class, GameDeveloperDao::class, GamePublisherDao::class), version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun gamesDao(): GameDaoService
--- app/src/main/java/com/piticlistudio/playednext/data/entity/dao/DaoModels.kt
+++ app/src/main/java/com/piticlistudio/playednext/data/entity/dao/DaoModels.kt
@@ -42,6 +42,13 @@ data class CompanyDao(@PrimaryKey val id: Int, val name: String, val slug: Strin
                 (ForeignKey(entity = CompanyDao::class, parentColumns = arrayOf("id"), childColumns = arrayOf("companyId"), onDelete = ForeignKey.CASCADE))))
 data class GameDeveloperDao(val gameId: Int, val companyId: Int)
 
+@Entity(tableName = "game_publisher",
+        primaryKeys = arrayOf("gameId", "companyId"),
+        foreignKeys = arrayOf(
+                (ForeignKey(entity = GameDao::class, parentColumns = arrayOf("id"), childColumns = arrayOf("gameId"), onDelete = ForeignKey.CASCADE)),
+                (ForeignKey(entity = CompanyDao::class, parentColumns = arrayOf("id"), childColumns = arrayOf("companyId"), onDelete = ForeignKey.CASCADE))))
+data class GamePublisherDao(val gameId: Int, val companyId: Int)
+
 //@Entity(tableName = "time_to_beat",
 //        foreignKeys = arrayOf(ForeignKey(entity = GameDao::class,
 //                parentColumns = arrayOf("id"),
