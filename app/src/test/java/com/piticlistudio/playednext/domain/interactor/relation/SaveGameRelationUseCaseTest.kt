9bda69e68162ec0ddfbe92e40276f8c849fd754d
Jorge <piticlistudio@gmail.com>
2017-11-06 11:53:58 +0100

    Add SaveGameRelationUseCase
    Adds usecase for storing game relation entities.

---
+++ app/src/test/java/com/piticlistudio/playednext/domain/interactor/relation/SaveGameRelationUseCaseTest.kt
@@ -0,0 +1,64 @@
+package com.piticlistudio.playednext.domain.interactor.relation
+
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import com.piticlistudio.playednext.domain.repository.GameRelationRepository
+import com.piticlistudio.playednext.test.factory.GameRelationFactory.Factory.makeGameRelation
+import io.reactivex.Completable
+import io.reactivex.observers.TestObserver
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+internal class SaveGameRelationUseCaseTest {
+
+    @Nested
+    @DisplayName("Given a SaveGameRelationUseCase")
+    inner class instance {
+
+        private lateinit var usecase: SaveGameRelationUseCase
+        @Mock private lateinit var repository: GameRelationRepository
+
+        @BeforeEach
+        internal fun setUp() {
+            MockitoAnnotations.initMocks(this)
+            usecase = SaveGameRelationUseCase(repository)
+        }
+
+        @Nested
+        @DisplayName("When we call execute")
+        inner class executeIsCalled {
+
+            private var observer: TestObserver<Void>? = null
+            private val relation = makeGameRelation()
+
+            @BeforeEach
+            internal fun setUp() {
+                whenever(repository.save(any())).thenReturn(Completable.complete())
+                observer = usecase.execute(relation).test()
+            }
+
+            @Test
+            @DisplayName("Then should request repository")
+            fun callsRepository() {
+                verify(repository).save(relation)
+            }
+
+            @Test
+            @DisplayName("Then emits without errors")
+            fun withoutErrors() {
+                assertNotNull(observer)
+                observer?.apply {
+                    assertNoErrors()
+                    assertNoValues()
+                    assertComplete()
+                }
+            }
+        }
+    }
+}
