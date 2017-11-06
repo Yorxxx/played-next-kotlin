222bcebfd25b6d94b00466d72cab9abff2733d6f
Jorge <piticlistudio@gmail.com>
2017-11-06 09:02:03 +0100

    [WIP] ViewModel testing
    Testing ViewModels in GameRelationLoad

--- app/src/main/java/com/piticlistudio/playednext/features/FooActivity.kt
+++ app/src/main/java/com/piticlistudio/playednext/features/FooActivity.kt
@@ -1,9 +1,14 @@
 package com.piticlistudio.playednext.features
 
 import android.app.Fragment
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.piticlistudio.playednext.R
+import com.piticlistudio.playednext.domain.model.GameRelation
+import com.piticlistudio.playednext.features.gamerelation.load.GameRelationLoadViewModel
 import com.piticlistudio.playednext.ui.game.load.GameLoadFragment
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
@@ -23,6 +28,23 @@ class FooActivity : AppCompatActivity(), HasFragmentInjector {
         fragmentManager.beginTransaction()
                 .add(android.R.id.content, GameLoadFragment())
                 .commit()
+
+        val viewmodel = ViewModelProviders.of(this).get(GameRelationLoadViewModel::class.java)
+        viewmodel.loading.observe(this, Observer<Boolean> {
+            Log.d("FooActivity", "Is loading ${it}")
+        })
+
+        viewmodel.error.observe(this, Observer<Throwable?> {
+            it?.let {
+                Log.d("FooActivity", "Showing error ${it}")
+            }
+        })
+
+        viewmodel.relation.observe(this, Observer<GameRelation?> {
+            it?.let {
+                Log.d("FooActivity", "Showing relation ${it}")
+            }
+        })
     }
 
     override fun fragmentInjector(): AndroidInjector<Fragment> {
