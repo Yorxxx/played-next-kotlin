5764ce43107e2def796a1b936b0d7a4c2f7735f3
Jorge <piticlistudio@gmail.com>
2017-11-06 17:27:42 +0100

    [WIP] Fix GameRepositoryImpl test case

--- app/src/test/java/com/piticlistudio/playednext/data/repository/GameRepositoryImplTest.kt
+++ app/src/test/java/com/piticlistudio/playednext/data/repository/GameRepositoryImplTest.kt
@@ -10,9 +10,11 @@ import com.piticlistudio.playednext.data.repository.datasource.net.GameRemoteImp
 import com.piticlistudio.playednext.domain.model.Game
 import com.piticlistudio.playednext.test.factory.GameFactory.Factory.makeGame
 import com.piticlistudio.playednext.util.RxSchedulersOverrideRule
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Completable
-import io.reactivex.Single
+import io.reactivex.Flowable
 import io.reactivex.observers.TestObserver
+import io.reactivex.subscribers.TestSubscriber
 import org.junit.Rule
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
@@ -46,20 +48,21 @@ internal class GameRepositoryImplTest {
             repository = GameRepositoryImpl(remoteImpl, localImpl)
         }
 
-        /*@Nested
+        @Nested
         @DisplayName("When we call load")
         inner class Load {
             val id = 10
             val entity = makeGame().apply {
                 syncedAt = System.currentTimeMillis()
             }
-            var result: TestObserver<Game>? = null
+            var result: TestSubscriber<Game>? = null
 
             @BeforeEach
             fun setup() {
-                whenever(localImpl.load(anyInt())).thenReturn(Single.just(entity))
+                val flowable = Flowable.create<Game>({ it.onNext(entity) }, BackpressureStrategy.MISSING)
+                whenever(localImpl.load(anyInt())).thenReturn(flowable)
                 whenever(localImpl.save(entity)).thenReturn(Completable.complete())
-                whenever(remoteImpl.load(anyInt())).thenReturn(Single.just(entity))
+                whenever(remoteImpl.load(anyInt())).thenReturn(flowable)
                 result = repository?.load(id)?.test()
             }
 
@@ -82,14 +85,14 @@ internal class GameRepositoryImplTest {
                 result?.apply {
                     assertNoErrors()
                     assertValueCount(1)
-                    assertComplete()
+                    assertNotComplete()
                     assertValue(entity)
                 }
             }
 
             @Nested
             @DisplayName("And local repository has unsynced data")
-            inner class localDataIsOld {
+            inner class LocalDataIsOld {
 
                 @BeforeEach
                 internal fun setUp() {
@@ -107,11 +110,11 @@ internal class GameRepositoryImplTest {
                 @DisplayName("Then should emit without errors")
                 fun withoutErrors() {
                     assertNotNull(result)
-                    with(result) {
-                        this?.assertNoErrors()
-                        this?.assertValueCount(1)
-                        this?.assertComplete()
-                        this?.assertValue(entity)
+                    result?.apply {
+                        assertNoErrors()
+                        assertValueCount(1)
+                        assertNotComplete()
+                        assertValue(entity)
                     }
                 }
 
@@ -123,11 +126,12 @@ internal class GameRepositoryImplTest {
 
                 @Nested
                 @DisplayName("And request fails")
-                inner class refreshFailed {
+                inner class RefreshFailed {
 
                     @BeforeEach
                     internal fun setUp() {
-                        whenever(remoteImpl.load(anyInt())).thenReturn(Single.error(Throwable("No Internet Connection")))
+                        val flowable = Flowable.create<Game>({ it.onError(Throwable("No Internet connection")) }, BackpressureStrategy.MISSING)
+                        whenever(remoteImpl.load(anyInt())).thenReturn(flowable)
                         result = repository?.load(id)?.test()
                     }
 
@@ -135,11 +139,11 @@ internal class GameRepositoryImplTest {
                     @DisplayName("Then should emit without errors")
                     fun withoutErrors() {
                         assertNotNull(result)
-                        with(result) {
-                            this?.assertNoErrors()
-                            this?.assertValueCount(1)
-                            this?.assertComplete()
-                            this?.assertValue(entity)
+                        result?.apply {
+                            assertNoErrors()
+                            assertValueCount(1)
+                            assertValue(entity)
+                            assertNotComplete()
                         }
                     }
                 }
@@ -147,12 +151,15 @@ internal class GameRepositoryImplTest {
 
             @Nested
             @DisplayName("And there is no result in local repository")
-            inner class withoutLocalResult {
+            inner class WithoutLocalResult {
 
                 @BeforeEach
                 internal fun setUp() {
-                    whenever(localImpl.load(id)).thenReturn(Single.error(EmptyResultSetException("no results")))
-                    whenever(remoteImpl.load(id)).thenReturn(Single.just(entity))
+                    val localFlow = Flowable.create<Game>({ it.onError(EmptyResultSetException("No results")) }, BackpressureStrategy.MISSING)
+                    whenever(localImpl.load(id)).thenReturn(localFlow)
+
+                    val remoteFlow = Flowable.create<Game>({ it.onNext(entity) }, BackpressureStrategy.MISSING)
+                    whenever(remoteImpl.load(id)).thenReturn(remoteFlow)
                     whenever(localImpl.save(entity)).thenReturn(Completable.complete())
                     result = repository?.load(id)?.test()
                 }
@@ -167,11 +174,11 @@ internal class GameRepositoryImplTest {
                 @DisplayName("Then should emit without errors")
                 fun withoutErrors() {
                     assertNotNull(result)
-                    with(result) {
-                        this?.assertNoErrors()
-                        this?.assertValueCount(1)
-                        this?.assertComplete()
-                        this?.assertValue(entity)
+                    result?.apply {
+                        assertNoErrors()
+                        assertValueCount(1)
+                        assertComplete()
+                        assertValue(entity)
                     }
                 }
 
@@ -188,14 +195,21 @@ internal class GameRepositoryImplTest {
         @DisplayName("When we call search")
         inner class Search {
 
-            val model1 = makeGame()
-            val model2 = makeGame()
-            var result: TestObserver<List<Game>>? = null
+            private val model1 = makeGame()
+            private val model2 = makeGame()
+            var result: TestSubscriber<List<Game>>? = null
 
             @BeforeEach
             fun setup() {
-                whenever(remoteImpl.search("query")).thenReturn(Single.just(listOf(model1, model2)))
+                val flow = Flowable.create<List<Game>>({ it.onNext(listOf(model1, model2)) }, BackpressureStrategy.MISSING)
+                whenever(remoteImpl.search("query")).thenReturn(flow)
                 result = repository?.search("query")?.test()
+            }
+
+            @Test
+            @DisplayName("Then should not request local")
+            fun localIsNotCalled() {
+                verifyZeroInteractions(localImpl)
             }
 
             @Test
@@ -208,11 +222,11 @@ internal class GameRepositoryImplTest {
             @DisplayName("Then should emit without errors")
             fun emissionWithoutErrors() {
                 assertNotNull(result)
-                with(result) {
-                    this?.assertNoErrors()
-                    this?.assertComplete()
-                    this?.assertValueCount(1)
-                    this?.assertValue(listOf(model1, model2))
+                result?.apply {
+                    assertNoErrors()
+                    assertValueCount(1)
+                    assertNoErrors()
+                    assertValue(listOf(model1, model2))
                 }
             }
         }
@@ -246,6 +260,6 @@ internal class GameRepositoryImplTest {
                     assertNoValues()
                 }
             }
-        }*/
+        }
     }
 }
