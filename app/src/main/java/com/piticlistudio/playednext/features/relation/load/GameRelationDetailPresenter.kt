118351311168d2789343f658b13fbd33711806a5
Jorge <piticlistudio@gmail.com>
2017-11-07 10:06:26 +0100

    Fix GameRelationDetailPresenter

--- app/src/main/java/com/piticlistudio/playednext/features/relation/load/GameRelationDetailPresenter.kt
+++ app/src/main/java/com/piticlistudio/playednext/features/relation/load/GameRelationDetailPresenter.kt
@@ -4,13 +4,36 @@ import com.piticlistudio.playednext.domain.interactor.relation.LoadGameRelationU
 import com.piticlistudio.playednext.domain.interactor.relation.SaveGameRelationUseCase
 import com.piticlistudio.playednext.domain.model.GameRelation
 import com.piticlistudio.playednext.features.base.BasePresenter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class GameRelationDetailPresenter @Inject constructor(private val loadUseCase: LoadGameRelationUseCase,
                                                       private val saveUseCase: SaveGameRelationUseCase) : BasePresenter<GameRelationDetailContract.View>(), GameRelationDetailContract.Presenter {
 
     override fun load(gameId: Int, platformId: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        checkViewAttached()
+        loadUseCase.execute(Pair(gameId, platformId))
+                .doOnSubscribe {
+                    mvpView?.showLoading()
+                    mvpView?.hideError()
+                    mvpView?.hideData()
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .toObservable()
+                .subscribeBy(
+                        onNext = {
+                            mvpView?.showData(it)
+                            mvpView?.hideLoading()
+                        },
+                        onError = {
+                            mvpView?.showError(it)
+                            mvpView?.hideLoading()
+                            mvpView?.hideData()
+                        }
+                )
     }
 
     override fun save(data: GameRelation) {
