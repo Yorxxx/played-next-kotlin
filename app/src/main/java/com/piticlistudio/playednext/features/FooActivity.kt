f186bb75b98abb5cfe9b1102978bd96f3ffb0374
Jorge <piticlistudio@gmail.com>
2017-11-08 10:02:10 +0100

    Add setContentFragment method
    Add extension function for AppCompatActivities that allow to set the content fragment if this fragment is not previously set.

--- app/src/main/java/com/piticlistudio/playednext/features/FooActivity.kt
+++ app/src/main/java/com/piticlistudio/playednext/features/FooActivity.kt
@@ -6,8 +6,8 @@ import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.piticlistudio.playednext.R
 import com.piticlistudio.playednext.ui.gamerelation.detail.GameRelationDetailFragment
+import com.piticlistudio.playednext.util.ext.setContentFragment
 import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
@@ -22,12 +22,8 @@ class FooActivity : AppCompatActivity(), HasSupportFragmentInjector {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_search_fragment)
-        supportFragmentManager.beginTransaction()
-                .add(android.R.id.content, GameRelationDetailFragment())
-                .commit()
+        setContentFragment(android.R.id.content, { GameRelationDetailFragment() })
     }
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
-        return fragmentInjector
-    }
+    override fun supportFragmentInjector() = fragmentInjector
 }
