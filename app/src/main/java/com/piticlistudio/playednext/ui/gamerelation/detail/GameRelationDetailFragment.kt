222c0fac292370e671ba64487740459a122fdc7e
Jorge <piticlistudio@gmail.com>
2017-11-17 13:48:39 +0100

    [WIP] Add databinding
    Adds databinding library to the project and some initial assessment into the GameRelationDetail view.

--- app/src/main/java/com/piticlistudio/playednext/ui/gamerelation/detail/GameRelationDetailFragment.kt
+++ app/src/main/java/com/piticlistudio/playednext/ui/gamerelation/detail/GameRelationDetailFragment.kt
@@ -132,10 +132,4 @@ class GameRelationDetailFragment : Fragment(), AnkoLogger {
 
         toolbar.setOnClickListener { appbar.setExpanded(true) }
     }
-
-    private fun showGame(data: Game) {
-        detail_content.visibility = View.VISIBLE
-        toolbar.title = data.name
-        backdropTitle.text = data.name
-    }
 }
