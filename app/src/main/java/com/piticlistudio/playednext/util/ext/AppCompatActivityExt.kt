7c52a4432147ed10ea4f43a7cf3e468495e6ed20
Jorge <piticlistudio@gmail.com>
2017-11-17 10:14:49 +0100

    Add screenshot automatic change
    View changes the screenshot image after five seconds.

--- app/src/main/java/com/piticlistudio/playednext/util/ext/AppCompatActivityExt.kt
+++ app/src/main/java/com/piticlistudio/playednext/util/ext/AppCompatActivityExt.kt
@@ -1,7 +1,11 @@
 package com.piticlistudio.playednext.util.ext
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Point
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
+import android.view.WindowManager
 
 inline fun FragmentActivity.setContentFragment(containerViewId: Int, f: () -> Fragment): Fragment? {
     val manager = supportFragmentManager
@@ -10,4 +14,13 @@ inline fun FragmentActivity.setContentFragment(containerViewId: Int, f: () -> Fr
     return f().apply {
         manager?.beginTransaction()?.add(containerViewId, this)?.commit()
     }
+}
+
+inline fun Activity.getScreenHeight(): Int {
+    (this.applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay?.let {
+        val size = Point()
+        it.getSize(size)
+        return size.y
+    }
+    return 0
 }
