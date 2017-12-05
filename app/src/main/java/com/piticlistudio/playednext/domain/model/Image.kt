9c4bcf2d9f7d06e64f9cd8a2523def8300feaaa9
Jorge <piticlistudio@gmail.com>
2017-12-05 09:42:58 +0100

    Fix GameImage was not generating correctly the expected urls
    GameImage was not taken into consideration that url values can have HTTP, HTTPS prefixes.

--- app/src/main/java/com/piticlistudio/playednext/domain/model/Image.kt
+++ app/src/main/java/com/piticlistudio/playednext/domain/model/Image.kt
@@ -2,7 +2,11 @@ package com.piticlistudio.playednext.domain.model
 
 data class GameImage constructor(val id: String, val url: String, val width: Int?, val height: Int?) {
 
-    val mediumSizeUrl: String = "http:${url}".replace("t_thumb", "t_screenshot_med")
-    val bigSizeUrl: String = "http:${url}".replace("t_thumb", "t_screenshot_big")
-    val hugeSizeUrl: String = "http:${url}".replace("t_thumb", "t_screenshot_huge")
+    val mediumSizeUrl: String = "https:${clearHTTPPrefix(url)}".replace("t_thumb", "t_screenshot_med")
+    val bigSizeUrl: String = "https:${clearHTTPPrefix(url)}".replace("t_thumb", "t_screenshot_big")
+    val hugeSizeUrl: String = "https:${clearHTTPPrefix(url)}".replace("t_thumb", "t_screenshot_huge")
+
+    private fun clearHTTPPrefix(input: String): String {
+        return input.removePrefix("http:").removePrefix("https:")
+    }
 }
