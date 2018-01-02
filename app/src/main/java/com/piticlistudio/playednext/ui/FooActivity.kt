145814ead3d8ead4cfb2d675d74e3c3a7522f5d5
Jorge <piticlistudio@gmail.com>
2018-01-02 13:22:03 +0100

    WIP initial commit

--- app/src/main/java/com/piticlistudio/playednext/ui/FooActivity.kt
+++ app/src/main/java/com/piticlistudio/playednext/ui/FooActivity.kt
@@ -1,13 +1,18 @@
 package com.piticlistudio.playednext.ui
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import com.piticlistudio.playednext.R
 import com.piticlistudio.playednext.ui.game.search.GameSearchFragment
 import com.piticlistudio.playednext.util.ext.setContentFragment
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 
@@ -18,7 +23,20 @@ class FooActivity : AppCompatActivity(), HasSupportFragmentInjector {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentFragment(android.R.id.content, { GameSearchFragment() })
+        setContentView(R.layout.activity_main)
+        setContentFragment(R.id.content, { GameSearchFragment() })
+    }
+
+    private fun initView() {
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_backlog -> {
+                    bottom_navigation_view.setBackgroundResource(R.drawable.bottom_navigation_main_backlog_selector)
+                    true
+                }
+                else -> { false }
+            }
+        }
     }
 
     override fun supportFragmentInjector() = fragmentInjector
